20c790511acabf61fcd9ed30a9b4341fec401793
mrsingh <mrsingh@ncsu.edu>
2015-10-27 06:28:02 -0400

    cleaned previous code

--- proxy.js
+++ proxy.js
@@ -18,50 +18,11 @@ var proxy_servers = []
 
 //Create proxy servers for each target url
 for (each_target_url in proxy_servers){
-	proxy_servers.push(http.createServer({target:each_target_url}));
+	proxy_servers.push(httpProxy.createProxyServer({target:each_target_url}));
 }
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-
-  client.del("host:port");
-  client.lpush("host:port","http://localhost:3001");
-  client.lpush("host:port","http://localhost:3002");
 
 
-  
-});
 
-console.log("listening on port 5722")
-server.listen(5722, function () {
-
-	  var host = server.address().address
-	  console.log("HOST IS="+host);
-	  var port = server.address().port
-
-	client.rpoplpush("host:port","host:port",function(err,resp){
-
-		if(err){
-			throw err;
-		}
-		else
-		{
-			console.log("response from rpoplplush:"+resp);
-			var new_http_url = resp;
-			console.log("new_http_url:="+new_http_url);
-			var proxy = httpProxy.createProxyServer({ target : new_http_url });
-			proxy.web(req, res);
-
-		}
-
-	})
-
-  // client.
 
   console.log('Example app listening at http://%s:%s', host, port)
 })
